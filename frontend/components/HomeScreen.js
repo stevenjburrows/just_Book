f45c67a221f62b90b234ac33e29f7cb3993ac281
adam <adam.lewispetrie@gmail.com>
2020-05-25 19:05:47 +0100

    adds new buttons

--- frontend/components/HomeScreen.js
+++ frontend/components/HomeScreen.js
@@ -1,21 +1,28 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Text, View, Button } from 'react-native';
-// import myImage from './assets/justBook.jpg';
-
+import { Text, View, Button } from "react-native";
+// import myImage from "./assets/justBook.jpg";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      
+    <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
+      <Text>justBook</Text>
+
       <Button
         title="Manage Bookings"
-        onPress={() => navigation.navigate('ManageBookings')}
+        onPress={() => navigation.navigate("ManageBookings")}
+      />
+
+      <Button
+        title="Manage Tables"
+        onPress={() => navigation.navigate("ManageTables")}
+      />
+      <Button
+        title="Manage Customers"
+        onPress={() => navigation.navigate("ManageCustomers")}
       />
     </View>
   );
-}
-
+};
 
 export default HomeScreen;
