89e27cd1081e957503f7638ab540e2f99c97fcc3
adam <adam.lewispetrie@gmail.com>
2020-05-27 17:09:48 +0100

    adds css

--- frontend/src/components/ManageBookings.js
+++ frontend/src/components/ManageBookings.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import { Text, View, Button } from "react-native";
-import Request from '../helpers/Request.js';
-import Booking from '../containers/Booking.js';
-import AddBookingForm from '../containers/AddBookingForm';
+import React, { Component } from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
+import Request from "../helpers/Request.js";
+import Booking from "../containers/Booking.js";
+import AddBookingForm from "../containers/AddBookingForm";
 
 class ManageBookings extends Component {
   constructor(props) {
     super(props);
     this.state = {
       bookings: [],
-    }
+    };
     this.fetchBookings = this.fetchBookings.bind(this);
   }
 
-  fetchBookings(){
+  fetchBookings() {
     const request = new Request();
-    request.get('http://localhost:8080/bookings')
-      .then((data) => {
-        this.setState({ bookings: data })
-      })
+    request.get("http://localhost:8080/bookings").then((data) => {
+      this.setState({ bookings: data });
+    });
   }
 
   componentDidMount() {
@@ -27,22 +26,47 @@ class ManageBookings extends Component {
 
   render() {
     return (
-      < View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-        <Button 
-        title="Add Booking"
-        onPress={() => this.props.navigation.navigate("AddBooking", {fetchBookings: this.fetchBookings})}
+      <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
+        <Button
+          title="Add Booking"
+          onPress={() =>
+            this.props.navigation.navigate("AddBooking", {
+              fetchBookings: this.fetchBookings,
+            })
+          }
         />
 
-
-        <Text>Manage Bookings</Text>
-        {
-          this.state.bookings.map((booking) => (
-            <Booking key={booking.id} booking={booking}></Booking>
-          ))
-        }
-      </View >
-    )
+        <Text style={styles.header}>Manage Bookings</Text>
+        {this.state.bookings.map((booking) => (
+          <Booking
+            style={{ borderWidth: 1, borderColor: "#000000" }}
+            key={booking.id}
+            booking={booking}
+          ></Booking>
+        ))}
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  bookinginfo: {
+    fontSize: 40,
+    alignSelf: "stretch",
+    height: 40,
+    paddingTop: 10,
+    textAlign: "center",
+    color: "#000000",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderBottomColor: "#000000",
+    borderBottomWidth: 0.5,
+    justifyContent: "center",
+  },
+
+  header: {
+    fontSize: 24,
+  },
+});
 
 export default ManageBookings;
--- frontend/src/containers/Booking.js
+++ frontend/src/containers/Booking.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const Booking = (props) => {
   return (
-    <>
-      <Text>Party Name: {props.booking.partyName}</Text>
-      <Text>Party Size: {props.booking.partySize}</Text>
-      <Text>Date: {props.booking.date}</Text>
-      <Text>Start: {props.booking.start}</Text>
-      <Text>End: {props.booking.end}</Text>
-
-    </>
-  )
-}
+    <View>
+      <View
+        style={{
+          borderBottomColor: "black",
+          borderBottomWidth: 2,
+        }}
+      />
+      <Text style={styles.bookinginfo}>
+        Party Name: {props.booking.partyName}
+      </Text>
+      <Text style={styles.bookinginfo}>
+        Party Size: {props.booking.partySize}
+      </Text>
+      <Text style={styles.bookinginfo}>Date: {props.booking.date}</Text>
+      <Text style={styles.bookinginfo}>Start: {props.booking.start}</Text>
+      <Text style={styles.bookinginfo}>End: {props.booking.end}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  bookingInfo: {
+  bookinginfo: {
     fontSize: 18,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     height: 40,
-    paddingTop: 10,
-    textAlign: 'center',
-    color: '#000000',
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderBottomColor: '#000000',
-    borderBottomWidth: 0.5,
-    justifyContent: 'center'
-  }
+    paddingTop: 5,
+    textAlign: "center",
+    color: "#000000",
+    justifyContent: "center",
+  },
+
+  applyLine: {
+    borderBottomColor: "#000000",
+    borderBottomWidth: 1,
+  },
 });
 
 export default Booking;
