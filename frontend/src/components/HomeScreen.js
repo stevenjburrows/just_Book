fc8f4ccb48542409b1a7e18f94d68c396672f142
adam <adam.lewispetrie@gmail.com>
2020-05-27 09:41:02 +0100

    adds fade in and image round

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,13 +1,51 @@
-import * as React from "react";
-import { Text, View, Button, Image } from "react-native";
+import React, { Component } from "react";
+import { Text, View, Button, Image, StyleSheet, Animated } from "react-native";
+
+class ImageLoader extends Component {
+  state = {
+    opacity: new Animated.Value(0),
+  };
+
+  onLoad = () => {
+    Animated.timing(this.state.opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  render() {
+    return (
+      <Animated.Image
+        onLoad={this.onLoad}
+        {...this.props}
+        style={[
+          {
+            opacity: this.state.opacity,
+            transform: [
+              {
+                scale: this.state.opacity.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0.85, 1],
+                }),
+              },
+            ],
+          },
+          this.props.style,
+        ]}
+      />
+    );
+  }
+}
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-      <Image style={{ flex: 5}}
-        source={require('../../assets/JustBook.jpg')}
+    <View style={styles.bg}>
+      <ImageLoader
+        style={styles.logo}
+        source={require("../../assets/justBook.jpg")}
       />
-      <Text>justBook</Text>
+      <Text>Welcome.</Text>
 
       <Button
         title="Manage Bookings"
@@ -18,7 +56,7 @@ const HomeScreen = ({ navigation }) => {
         title="Manage Tables"
         onPress={() => navigation.navigate("ManageTables")}
       />
-      
+
       <Button
         title="Manage Customers"
         onPress={() => navigation.navigate("ManageCustomers")}
@@ -26,5 +64,27 @@ const HomeScreen = ({ navigation }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+
+  logo: {
+    width: 250,
+    height: 250,
+    resizeMode: "stretch",
+    borderColor: "#000000",
+    borderRadius: 250 / 2,
+    marginBottom: 80,
+  },
+
+  bg: {
+    backgroundColor: "#C1DBE3",
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default HomeScreen;
